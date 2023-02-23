6735b2d2e7d43db3de5660815f6ec0a820308e61
ArtemKuzyk <artem.brar@gmail.com>
2023-02-23 13:58:54 +0200

    add logic to the digital dial

--- src/components/clock-area/ClockArea.js
+++ src/components/clock-area/ClockArea.js
@@ -1,57 +1,74 @@
 import React, { useEffect } from 'react';
-import { Transition } from 'react-transition-group';
 import { useState } from 'react';
 import './clock-area.css';
+
 export const ClockArea = () => {
 
     const [initialBackgroundValues, setInitialBackgroundValues] = useState([0, 1, 2]);
+    //let initialBackgroundValues = [0, 1, 2];
     let initialBackground = `conic-gradient(white ${initialBackgroundValues[0]}deg,
                                             black ${initialBackgroundValues[1]}deg, 
                                             black ${initialBackgroundValues[1]}deg, 
                                             white ${initialBackgroundValues[2]}deg)`;
     const [background, setBackground] = useState(initialBackground);
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
 
     useEffect(() => {
-        const interval = setInterval(() => {
+        const secondInterval = setInterval(() => {
             setInitialBackgroundValues(prev => {
                 if(prev[0] === 360 - 6) {
                     return [0, 1, 2]
                 }
                 return prev.map(el => el + 6);
             });
+            
+            setSeconds(prev => {
+                if(prev === 59) return 0;
+                return prev + 1;
+            });
         }, 1000);
+
+        const minuteInterval = setInterval(() => {
+            setMinutes(prev => {
+                if (prev === 59) return 0;
+                return prev + 1;
+            })
+        }, 60 * 1000)
+
     }, []);
+
+
     
     useEffect(() => {
             setBackground(initialBackground);
+
     }, [initialBackgroundValues]);
 
     return(<>
-    <Transition in={true} timeout={1000}>
         <div className='clock-area' 
                         style={{background: `${background}`}} 
                         key={Math.random()}>
         </div>
-    </Transition>
         <div className='colck-number-area'>
             <div className='minutes'>
                 <div id='firstMinute'>
-                    <div id='firstMinutePartOne'>7</div>
-                    <div id='firstMinutePartTwo'>7</div>
+                    <div id='firstMinutePartOne'>{minutes.toString().length > 1 ? minutes.toString()[0] : 0}</div>
+                    <div id='firstMinutePartTwo'>{minutes.toString().length > 1 ? minutes.toString()[0] : 0}</div>
                 </div>
                 <div id='secondMinute'>
-                    <div id='secondMinutePartOne'>8</div>
-                    <div id='secondMinutePartTwo'>8</div>
+                    <div id='secondMinutePartOne'>{minutes.toString().length > 1 ? minutes.toString()[1] : minutes  }</div>
+                    <div id='secondMinutePartTwo'>{minutes.toString().length > 1 ? minutes.toString()[1] : minutes}</div>
                 </div>
             </div>
             <div className='seconds'>
                 <div id='firstSecond'>
-                    <div id='firstSecondPartOne'>3</div>
-                    <div id='firstSecondPartTwo'>3</div>
+                    <div id='firstSecondPartOne'>{seconds.toString().length > 1 ? seconds.toString()[0] : 0}</div>
+                    <div id='firstSecondPartTwo'>{seconds.toString().length > 1 ? seconds.toString()[0] : 0}</div>
                 </div>
                 <div id='secondSecond'>
-                    <div id='secondSecondPartOne'>5</div>
-                    <div id='secondSecondPartTwo'>5</div>
+                    <div id='secondSecondPartOne'>{seconds.toString().length > 1 ? seconds.toString()[1] : seconds}</div>
+                    <div id='secondSecondPartTwo'>{seconds.toString().length > 1 ? seconds.toString()[1] : seconds}</div>
                 </div>
             </div>
         </div>
